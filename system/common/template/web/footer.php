45c023aa86bc2f16b301480abd068de0b863b8d5
liuyuhua <lyh@163.com>
2017-06-12 13:56:37 +0800

    init

---
+++ system/common/template/web/footer.php
@@ -0,0 +1,8 @@
+<?php defined('SYSTEM_IN') or exit('Access Denied');?>	<div id="footer" style="float: left;overflow: hidden;padding-top: 20px;margin: 20px 15px 20px 15px;border-top: 1px #eee solid;line-height: 20px;font-size: 14px;color: #666;">
+
+
+	</div>
+	
+</body>
+</html>
+
